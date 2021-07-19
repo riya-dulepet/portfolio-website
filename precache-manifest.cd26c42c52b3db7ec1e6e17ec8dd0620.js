07ab3a9de76982d8c70d4b90a30b46e1d00d1bd4
riya_dulepet@brown.edu <rajivdulepet@rajivs-mbp.lan>
2021-07-18 23:44:41 -0700

    Updates

---
+++ precache-manifest.cd26c42c52b3db7ec1e6e17ec8dd0620.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc74814f448abb3c51028b525b993b6",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "35a6bd98a61be3efa4ce",
+    "url": "/portfolio-website/static/css/main.1d47fd16.chunk.css"
+  },
+  {
+    "revision": "4257bc15aa2f3f876a85",
+    "url": "/portfolio-website/static/js/2.1614e17d.chunk.js"
+  },
+  {
+    "revision": "35a6bd98a61be3efa4ce",
+    "url": "/portfolio-website/static/js/main.dbcdf285.chunk.js"
+  },
+  {
+    "revision": "69d2a08512e791193171",
+    "url": "/portfolio-website/static/js/runtime-main.8019c3aa.js"
+  }
+]);
