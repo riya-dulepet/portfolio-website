ff8062fbcc145126d354703cc5824e8fc77c6a0a
riya_dulepet@brown.edu <rajivdulepet@rajivs-mbp.lan>
2021-07-18 20:16:08 -0700

    added base app

---
+++ src/components/Layout/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import { Content } from 'carbon-components-react/lib/components/UIShell'
+
+export const StyledContent = styled(Content)`
+  min-height: 100vh;
+
+  @media (max-width: 640px) {
+    margin-left: 0 !important;
+  }
+    background-color: #FFFFFF;
+  font-family: 'Merriweather', serif;
+color: black
+
+`
---
+++ src/pages/Me/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const ProfileLink = styled.li`
+  margin-right: 0.5rem;
+  margin-bottom: 0.5rem;
+  display: inline-block;
+  font-size: 18px;
+  color: black
+
+
+a {
+    color: black
+
+}
+
+`
+
+export const HeadLine = styled.p`
+  margin-top: 1.5rem;
+  font-family: 'Merriweather', serif;
+  font-style: italic;
+
+  font-size: x-large;
+`;
